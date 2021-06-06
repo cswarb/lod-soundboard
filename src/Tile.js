9071ad5f9fd459ee5efd8efba1f6fc03c0e10abf
Chris Swarbrick <chris.swarb@gmail.com>
2021-06-06 12:09:08 +0100

    Nice one mate

--- src/Tile.js
+++ src/Tile.js
@@ -1,8 +1,6 @@
 const playSound = (elem, sound) => {
     return () => {
-        console.log(elem.current, sound);
         const audioElem = elem.current;
-        console.log(audioElem);
 
         audioElem.src = sound.source;
         audioElem.volume = 0.5;
@@ -13,12 +11,63 @@ const playSound = (elem, sound) => {
     }
 };
 
+document.addEventListener('DOMContentLoaded', function () {
+    var lazyloadImages;
+
+    if ('IntersectionObserver' in window) {
+        lazyloadImages = document.querySelectorAll('.lazy');
+        var imageObserver = new IntersectionObserver(function (entries, observer) {
+            entries.forEach(function (entry) {
+                if (entry.isIntersecting) {
+                    var image = entry.target;
+                    image.classList.remove('lazy');
+                    image.setAttribute('style', `background-image: ${image.getAttribute('data-image')}`);
+                    imageObserver.unobserve(image);
+                }
+            });
+        });
+
+        lazyloadImages.forEach(function (image) {
+            imageObserver.observe(image);
+        });
+    } else {
+        var lazyloadThrottleTimeout;
+        lazyloadImages = document.querySelectorAll('.lazy');
+
+        function lazyload() {
+            if (lazyloadThrottleTimeout) {
+                clearTimeout(lazyloadThrottleTimeout);
+            }
+
+            lazyloadThrottleTimeout = setTimeout(function () {
+                var scrollTop = window.pageYOffset;
+                lazyloadImages.forEach(function(img) {
+                    if (img.offsetTop < (window.innerHeight + scrollTop)) {
+                        img.src = img.dataset.src;
+                        img.classList.remove('lazy');
+                        img.setAttribute('style', `background-image: ${img.getAttribute('data-image')}`);
+                    }
+                });
+                if (lazyloadImages.length === 0) {
+                    document.removeEventListener('scroll', lazyload);
+                    window.removeEventListener('resize', lazyload);
+                    window.removeEventListener('orientationChange', lazyload);
+                }
+            }, 20);
+        }
+
+        document.addEventListener('scroll', lazyload);
+        window.addEventListener('resize', lazyload);
+        window.addEventListener('orientationChange', lazyload);
+    }
+});
+
 const Tile = (props) => {
     return <button key={ props.i }
         type="button"
-        className="sound"
+        className="sound lazy"
         data-source={ props.sound.source }
-        style={ { backgroundImage: "url(" + props.sound.image + ")" } }
+        data-image={ "url(" + props.sound.image + ")" }
         onClick={ playSound(props.elem, props.sound) }>
         <span className="sound-text-container">
             <span className="sound-icon"></span>
